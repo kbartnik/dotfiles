51b0437986054f8499e8a7f849b2540048810b75
Kurt Bartnik <kurt@solidstatebrain.com>
2018-09-12 22:33:49 -0400

    add config for powerline9k theme

---
+++ shellrc/zshrc.d/05-powerlevel9k.zsh
@@ -0,0 +1,6 @@
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+POWERLEVEL9K_SHORTEN_STRATEGY="truncate_to_last"
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs history dir_writable vcs)
